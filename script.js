8aed028ed4e418a2e446388845ad0cd3e263730e
MHRDevS <muhammadhasan24620062020@gmail.com>
2026-01-21 17:15:42 +0500

    updated

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
-let display = document.getElementById('inputBox');
+var display = document.getElementById("inputBox");
 
 function appendValue(value) {
-    if (display.value === '0' || display.value === 'Error') {
+    if (display.value == "0") {
         display.value = value;
     } else {
-        display.value += value;
-    }
-}
-
-function calculateResult() {
-    try {
-        let expression = display.value.replace(/×/g, '*');
-        
-        expression = expression.replace(/(\d+)%/g, function(match, p1) {
-            return '(' + p1 + '/100)';
-        });
-        
-        let result = eval(expression);
-        
-        if (Number.isInteger(result)) {
-            display.value = result;
-        } else {
-            display.value = parseFloat(result.toFixed(8));
-        }
-    } catch (error) {
-        display.value = "Error";
+        display.value = display.value + value;
     }
 }
 
@@ -40,53 +20,29 @@ function backspace() {
     }
 }
 
-document.addEventListener('keydown', function(event) {
-    const key = event.key;
-    
-    if (key >= '0' && key <= '9') {
-        appendValue(key);
-    }
-    
-    else if (key === '+' || key === '-' || key === '*') {
-        appendValue(key);
-    }
-    else if (key === '/') {
-        event.preventDefault(); 
-        appendValue('/');
-    }
-    
-    else if (key === 'Enter' || key === '=') {
-        event.preventDefault();
-        calculateResult();
-    }
-    
-    else if (key === 'Escape' || key === 'c' || key === 'C') {
-        clearDisplay();
-    }
-    
-    else if (key === 'Backspace') {
-        event.preventDefault();
-        backspace();
-    }
-    
-    else if (key === '.') {
-        appendValue('.');
-    }
-    
-    else if (key === '%') {
-        appendValue('%');
-    }
-});
+function calculateResult() {
+    var exp = display.value;
 
-display.value = "0";
+    if (exp == "") {
+        display.value = "0";
+        return;
+    }
 
-function calculatePercentage() {
-    try {
-        let currentValue = parseFloat(display.value);
-        if (!isNaN(currentValue)) {
-            display.value = currentValue / 100;
-        }
-    } catch (error) {
+    exp = exp.replace("×", "*");
+
+    if (exp.indexOf("%") != -1) {
+        var num = exp.replace("%", "");
+        display.value = num / 100;
+        return;
+    }
+
+    var result = eval(exp);
+
+    if (result == Infinity || isNaN(result)) {
         display.value = "Error";
+    } else {
+        display.value = result;
     }
 }
+
+display.value = "0";
